8c397aa985c6edb152cd21b2a84c2c1d5def0047
Sebastian Murcia <s.murciag@uniandes.edu.co>
2024-10-02 13:00:17 -0500

    DB url added

--- src/session.py
+++ src/session.py
@@ -8,12 +8,13 @@ class SessionConfig():
         ...
 
     def url(self):
-        db_user = os.environ['DB_USER']
-        db_pass = os.environ['DB_PASSWORD']
-        db_host = os.environ['DB_HOST']
-        db_port = os.environ['DB_PORT']
-        db_name = os.environ['DB_NAME']
-        return f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+        # db_user = os.environ['DB_USER']
+        # db_pass = os.environ['DB_PASSWORD']
+        # db_host = os.environ['DB_HOST']
+        # db_port = os.environ['DB_PORT']
+        # db_name = os.environ['DB_NAME']
+        # return f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+        return os.environ['DATABASE_URL']
 
 
 session_config = SessionConfig()
